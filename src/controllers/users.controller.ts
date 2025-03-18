f309d019dcede261facc047fb80f19db43f38367
Gustavo Santana <gustavosantana@jspecas.com.br>
2025-03-18 19:24:01 -0300

    🚀 Adicionando código inicial do Ecommerce API

---
+++ src/controllers/users.controller.ts
@@ -0,0 +1,65 @@
+import { Request, Response } from "express";
+import {getFirestore} from "firebase-admin/firestore";
+
+
+type User= {
+    id: number;
+    nome: string;
+    email: string
+};
+
+
+export class UsersController{
+    static async getAll(req: Request, res: Response){
+       const snapshot=  await getFirestore().collection("users").get();
+       const users = snapshot.docs.map(doc =>{
+          return{
+            id:doc.id,
+            ...doc.data()
+          };
+       });
+        res.send(users);
+    }
+
+    static async getById(req: Request,res:Response){
+        let userId = (req.params.id);
+        const doc =  await getFirestore().collection("users").doc(userId).get();
+        res.send({
+            id: doc.id,
+            ...doc.data()
+        });
+    }
+
+    static async save(req: Request, res: Response){
+        let user = req.body;
+        const userSalvo = await getFirestore().collection("users").add(user);
+        res.send({
+            message: `Usuário ${userSalvo.id} criado com sucesso!`
+        });
+    }
+
+    static update (req: Request, res: Response)  {
+        let userId = req.params.id;
+        let user = req.body as User;
+        
+        getFirestore().collection("users").doc(userId).set({
+            nome: user.nome,
+            email:user.email
+        });
+
+        res.send({
+            message: "Usuário alterado com sucesso!"
+        })
+    }
+
+    static async delete (req: Request, res: Response)  {
+        let userId  = req.params.id;
+
+        getFirestore().collection("users").doc(userId).delete();
+        await getFirestore().collection("users").doc(userId).delete();
+
+        res.send({
+            message: "Usuário excluído com sucesso!"
+        })
+        }
+}
---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import express from "express";
+import { initializeApp } from "firebase-admin/app";
+import {routes} from "./routes/index";
+
+initializeApp();
+const app = express();
+
+routes(app);
+
+app.listen(3000, () => {
+    console.log("servidor Ativo na porta 3000")
+});
